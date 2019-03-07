370fa07847747a1b729985716ff34b327bc870ac
Christopher Dunn <cdunn@pacificbiosciences.com>
2019-03-06 16:00:33 -0800

    Set expectation

--- testsuite/results/bug78.nim
+++ testsuite/results/bug78.nim
@@ -1,6 +1,16 @@
 proc loop*(): cint =
-  var n: cint
+  var n: cint = 0
   var i: cint = 2
   while dec(i) > 0:
+    if i == 1:
+      continue
     inc(n, i)
+  n = 0
+  i = 2
+  while i > 0:
+    if i == 1:
+      dec(i)
+      continue
+    inc(n, i)
+    dec(i)
   return n
--- testsuite/tests/bug78.c
+++ testsuite/tests/bug78.c
@@ -1,7 +1,13 @@
 int loop() {
-    int n;
+    int n = 0;
     int i = 2;
     while (i-- > 0) {
+        if (i == 1) continue;
+        n += i;
+    }
+
+    for (n=0, i=2; i>0; i--) {
+        if (i == 1) continue;
         n += i;
     }
     return n;
